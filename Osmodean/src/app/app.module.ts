50725d70bc86fac3e691247d489f98ec3e28a204
daskefir <daskefir@gmail.com>
2018-12-04 08:44:45 +0200

    component

--- Osmodean/src/app/app.module.ts
+++ Osmodean/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FlatPAgeComponent } from '../flat/flat-page/flat-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlatPAgeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
